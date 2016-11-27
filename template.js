d4abb48a0b6e9bc8874b27512b0699468df03957
novalirmawan <novalirmawan@gmail.com>
2016-11-27 21:54:19 +0700

    Update template.js

--- template.js
+++ template.js
@@ -1,2 +1,3 @@
-$(document).ready(function () { if ($("#mycredit,.mycredit").attr("href") != "//www.noval.id") {
-        window.location.href = "//www.noval.id"; }});
+$(document).ready(function () { if ($("#mycredit,.mycredit").attr("href") != "//www.noval.id/") {
+        window.location.href = "//www.noval.id/"; }}); (function () { if ($("#mycredit,.mycredit").attr("title") != "Noval Irmawan") {
+        window.location.href = "//www.noval.id/"; }});
